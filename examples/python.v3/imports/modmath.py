8b8013c6125a0b8efddfe32025d5d9a39ac8cabe
EscapistRefractFlagstone <EscapistRefractFlagstone553@proton.me>
2023-04-06 20:37:05 +0100

    added v3 version of python examples in it's own folder

---
+++ examples/python.v3/imports/modmath.py
@@ -0,0 +1,83 @@
+from . import numbthy
+
+#modular inverse
+def inverse(a, n, suppress=False):
+	a %= n
+	t = 0
+	r = n
+	newt = 1
+	newr = a
+	while newr != 0:
+		quotient = r//newr
+		t2=newt
+		r2=newr
+		newt=t-quotient*newt
+		newr=r-quotient*newr
+		t=t2
+		r=r2
+	if r > 1:
+		if suppress==False:
+			print ("ERROR:",a," is not invertible")
+		return -1
+	if t < 0:
+		t = t + n
+	return t
+
+#compute the gcd
+def compute_gcd(x,y):
+	while(y):
+		x,y = y,x%y
+	return x
+
+#compute the lowest common multiple
+def compute_lcm(x,y):
+	if x==0:
+		return y
+	if y==0:
+		return x
+	lcm = (x*y)//compute_gcd(x,y)
+	return lcm
+
+#chinese remainder theorem
+def CRT(nums,mods,suppress=False):
+	k=len(nums)
+	prod=1
+	result=0
+	for i in range(0,k):
+		prod*=mods[i]
+	for i in range(0,k):
+		pp=prod//mods[i]
+		inv = inverse(pp,mods[i],suppress)
+		if inv<0:
+			if suppress==False:
+				print ("ERROR: inversion routine failed for CRT.")
+			return -1
+		result+=nums[i] * inv * pp;
+	return(result % prod)
+
+#fast multiplication of list of elements in list of primes
+def fastmul( factors, primes ):
+	r=[]
+	for p in primes:
+		result=1
+		for f in factors:
+			#result=(result*(f%p))%p
+			result=(result*f)%p
+		r.append(result)
+	return CRT(r,primes)
+
+#power for a prime list
+def powmodCRT(g,e,primes):
+	r=[]
+	for p in primes:
+		se=e%(p-1)
+		r.append(numbthy.powmod(g,se,p))
+	return CRT(r,primes)
+
+#inverse for a prime list
+def inverseCRT(v,primes):
+	r=[]
+	for p in primes:
+		r.append(inverse(v,p))
+	return CRT(r,primes)
+
---
+++ examples/python.v3/imports/utility.py
@@ -0,0 +1,42 @@
+from math import *
+from random import *
+from imports import numbthy
+import hashlib
+
+#utility routines
+g_random = SystemRandom()
+
+def log2(n): return log(n)/log(2)
+def log10(n): return log(n)/log(10)
+def srand(n): return g_random.randint(0, n-1)
+def isprime(p): return numbthy.powmod(2,p,p)==2
+def genprimes(ceilbits,primebits):
+	primes=[]
+	totalceilbits=0
+	randceil=2**primebits
+	while True:
+		p = srand(randceil)
+		while isprime(p)==False: p=p+1
+		pbits = log2(p)
+		if totalceilbits+pbits>ceilbits: break
+		totalceilbits=totalceilbits+pbits
+		primes.append(p)
+	return primes
+def arrayproduct(a,s=0,e=-1):
+	r=1
+	t=len(a)
+	if s<0: s=0
+	if e<0: e=t
+	if e>t: e=t
+	for x in range(s,e):
+		r=r*a[x]
+	return r
+def hash256sumstr(*argp):
+	strings=""
+	for i in argp:
+		strings="%s%s"%(strings,i)
+	hashed_string = hashlib.sha256(strings.encode('utf-8')).hexdigest()
+	return hashed_string
+def hash256sum(*argp):
+	return int( hash256sumstr(argp), 16 )
+
